83fdeb8bc9acaaa70c421768971ee15aecca0a81
haihustk63 <haiphamtk13clc@gmail.com>
2022-12-18 23:43:58 +0700

    :+1: Create post detail

--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -1,38 +1,89 @@
 <template>
   <div>
-    <p v-if="!post">Loading...</p>
-    <div v-else class="post-item">
-      <p>{{ post.id }}. {{ post.title }}</p>
-      <p>{{ post.body }}</p>
+    <p v-if="!post || !user">Loading...</p>
+    <div v-else class="post-detail">
+      <h2 class="title">{{ post.title }}</h2>
+      <h4 class="author">Author: {{ user.name }}</h4>
+      <p class="content">{{ post.body }}</p>
+      <div class="groupbtn">
+        <router-link :to="`/posts/${previousPostId}`"
+          ><button class="button">&larr; Previous</button></router-link
+        >
+        <router-link :to="`/posts/${nextPostId}`"
+          ><button class="button">Next &rarr;</button></router-link
+        >
+      </div>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { storeToRefs } from "pinia";
+import { onBeforeMount, ref, defineProps, computed, watch } from "vue";
+import { useRoute } from "vue-router";
 import { kyApi } from "../config/api";
+import { usePostStore } from "../store/posts";
 
-export default {
-  name: "PostDetail",
-  data: () => {
-    return {
-      post: undefined,
-    };
-  },
+const store = usePostStore();
+const route = useRoute();
 
-  async created() {
-    const postId = this.$route.params.postId;
+const post = ref({});
+const user = ref({});
+const { total } = storeToRefs(store);
+
+const handleGetPost = async () => {
+  try {
+    const { postId } = route.params;
     const resPost = await kyApi.get(`posts/${postId}`).json();
-    this.post = resPost;
-  },
+    const resUser = await kyApi.get(`users/${resPost.userId}`).json();
+    post.value = resPost;
+    user.value = resUser;
+  } catch (e) {}
 };
+
+onBeforeMount(handleGetPost);
+
+const nextPostId = computed(() => {
+  return post.value.id < total.value ? post.value.id + 1 : post.value.id;
+});
+
+const previousPostId = computed(() => {
+  return post.value.id > 1 ? post.value.id - 1 : post.value.id;
+});
 </script>
 
-<style scoped>
-.post-item {
+<style scoped lang="scss">
+.post-detail {
   background: #fefcf3;
   border-radius: 5px;
   margin: 10px;
-  padding: 10px;
+  padding: 20px;
   text-align: left;
+
+  > .title {
+    background-color: #f0dbdb;
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  > .author {
+    margin: 10px 0 20px;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  > .groupbtn {
+    display: flex;
+    gap: 10px;
+    margin-top: 20px;
+  }
+
+  > .groupbtn .button {
+    background-color: #dba39a;
+    height: fit-content;
+    width: 120px;
+    padding: 5px 10px;
+    outline: none;
+  }
 }
 </style>
