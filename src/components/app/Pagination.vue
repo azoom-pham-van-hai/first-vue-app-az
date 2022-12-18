e9e081137d883122ee619ff8fe80c25265d57efb
haihustk63 <haiphamtk13clc@gmail.com>
2022-12-18 23:44:18 +0700

    :+1: Create pagination

--- src/components/app/Pagination.vue
+++ src/components/app/Pagination.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="pagination"></div>
+  <div class="app-pagination">
+    <div v-for="i in pagesCount" @click="setPage(i)" class="item">{{ i }}</div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Pagination",
-  props: {
-    
-  },
-};
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["total", "setPage"]);
+
+const pagesCount = computed(() => {
+  return props.total / 10;
+});
 </script>
 
-<style scoped>
-.post-item {
-  background: #fefcf3;
-  box-shadow: 0 0 2px #dba39a;
-  border-radius: 5px;
-  margin: 10px;
-  padding: 10px;
-  text-align: left;
-  cursor: pointer;
+<style scoped lang="scss">
+.app-pagination {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  width: fit-content;
+  margin: auto;
+
+  > .item {
+    width: 40px;
+    height: 40px;
+    margin: 10px;
+    border-radius: 50%;
+    background-color: #dba39a;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  > .item:active {
+    transform: translateY(3px);
+    background-color: #d7604e;
+  }
 }
 </style>
